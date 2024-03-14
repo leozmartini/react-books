5ebf5f8fb5c85c6d67a2d46ebba512e162d0011a
demar-nbk <demartinileo@hotmail.com>
2024-03-14 03:37:13 -0300

    Criando GlobalStyle

--- src/App.js
+++ src/App.js
@@ -2,16 +2,9 @@ import styled from 'styled-components';
 import Header from './components/Header'
 
 const AppContainer = styled.div`
-
-    width: 100vw;
-    height: 100vh;
-    background-image: linear-gradient(90deg, #002F52 35%, #326589);
-  
-
-  li {
-    list-style: none;
-  }
-   
+  width: 100vw;
+  height: 100vh;
+  background-image: linear-gradient(90deg, #002F52 35%, #326589);
 `
 
 
