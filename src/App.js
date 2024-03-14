a6e0416d004b2ca57082aa8b637e4e15c01ab0da
demar-nbk <demartinileo@hotmail.com>
2024-03-14 02:32:35 -0300

    icones adicionados + Ajustes CSS

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import './App.css';
 import Logo from './components/Logo'
+import perfil from './images/perfil.svg'
+import sacola from './images/sacola.svg'
 
 const textOptions = ['CATEGORIAS', 'FAVORITOS', 'MINHA ESTANTE']
+const icons = [perfil, sacola]
+
 
 function App() {
   return (
@@ -13,6 +17,13 @@ function App() {
             <li className='option'><p>{text}</p></li>
           ))}
         </ul>
+
+        <ul className='icons'>
+          { icons.map( (icon) => (
+            <li className='icon'><img src={icon} alt='icon'></img></li>
+          ) )}
+        </ul>
+
       </header>
     </div>
   );
