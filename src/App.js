64c76bd4c6eccdd4efd264243f6ef3af4e18b30f
demar-nbk <demartinileo@hotmail.com>
2024-03-14 02:24:58 -0300

    Opcoes listadas com .map

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 import Logo from './components/Logo'
 
+const textOptions = ['CATEGORIAS', 'FAVORITOS', 'MINHA ESTANTE']
+
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'> 
+      <header className='App-header'>
         <Logo></Logo>
+        <ul className='options'>
+          {textOptions.map((text) => (
+            <li className='option'><p>{text}</p></li>
+          ))}
+        </ul>
       </header>
     </div>
   );
