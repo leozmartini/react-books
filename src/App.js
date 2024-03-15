db0e062bf330945e15c57f0d95858df52459ae6d
demar-nbk <demartinileo@hotmail.com>
2024-03-15 00:52:23 -0300

    Estilos do Search adicionados

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import Header from './components/Header'
+import Search from './components/Search';
 
 const AppContainer = styled.div`
   width: 100vw;
@@ -12,6 +13,7 @@ function App() {
   return (
     <AppContainer>
       <Header/>
+      <Search />
     </AppContainer>
   );
 }
