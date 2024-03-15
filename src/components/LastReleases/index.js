3b121311eef53f41e04a168ef7d14f63c9b57522
demar-nbk <demartinileo@hotmail.com>
2024-03-15 13:35:57 -0300

    Aba de recomendações adicionada

--- src/components/LastReleases/index.js
+++ src/components/LastReleases/index.js
@@ -1,4 +1,7 @@
 import { books } from './LastReleasesData'
+import {Title} from '../Title'
+import RecommendedCard from '../RecommendedCard'
+import imageBook from '../../images/livro2.png'
 import styled from 'styled-components'
 
 const LastReleasesContainer = styled.section`
@@ -6,17 +9,6 @@ background-color: #EBECEE;
 padding-bottom: 20px;
 display: flex;
 flex-direction: column;
-`
-
-
-const Title = styled.h2`
-width: 100%;
-padding: 30px 0;
-background-color: #FFF;
-color: #EB9B00;
-font-size: 36px;
-text-align: center;
-margin: 0;
 `
 
 const NewBooksContainer = styled.div`
@@ -31,7 +23,10 @@ cursor: pointer;
 function LastReleases() {
     return (
         <LastReleasesContainer>
-            <Title>Últimos lançamentos</Title>
+            <Title 
+                color='#EB9B00'    
+                fontSize='36px' 
+            >Últimos lançamentos</Title>
             <NewBooksContainer>
                 {
                     books.map(book => (
@@ -39,6 +34,14 @@ function LastReleases() {
                     ))
                 }
             </NewBooksContainer>
+
+            <RecommendedCard 
+                title="Talvez você se interesse por"
+                subtitle="Angular 11"
+                description="Construindo uma aplicação com a plataforma Google."
+                img={imageBook}
+            />
+
         </LastReleasesContainer>
     )
 }
